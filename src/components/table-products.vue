096218ca0aba7702b618d72340b8aaa0e7bdf19c
jabinherrera <j.herrera20@ufromail.cl>
2023-06-09 12:10:38 -0400

    tabla productos

--- src/components/table-products.vue
+++ src/components/table-products.vue
@@ -1,5 +1,22 @@
 <template>
-  
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Producto</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Cantidad</th>
+      <th scope="col">Subtotal</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
 </template>
 
 <script>
