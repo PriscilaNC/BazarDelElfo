8fe6c71ff2f4665dcf5a14622ab9970de06b45d2
jabinherrera <j.herrera20@ufromail.cl>
2023-06-09 16:13:29 -0400

    a

--- src/pages/MarketPlace.vue
+++ src/pages/MarketPlace.vue
@@ -1,36 +1,17 @@
 <template>
-  <HeaderComponent/>
-  <NavBar/>
-  <div v-for="product in products" v-bind:key="product.id">
-    <CardProduct/>
+  <div >
+    <Cardproduct v-for="product in products" v-bind:key="product.id"/>
   </div>
 </template>
 
 <script>
-//import {products} from '../components/card-producto.vue';
+import {products} from '../components/card-producto.vue';
 import CardProduct from '../components/card-producto.vue';
-import HeaderComponent from '@/components/header-component.vue';
-import NavBar from '@/components/nav-bar.vue';
 
 export default {
   name: 'MarketPlace',
-  components: { CardProduct, HeaderComponent, NavBar},
-  data() {
-    return {
-      product: Object,
-
-    };
-  },
-  mounted() {
-    this.verProducto();
-  },
-  methods: {
-    async verProducto() {
-        
-    },
-  },
-};
-
+  components: { CardProduct}
+}
 </script>
 
 <style>
