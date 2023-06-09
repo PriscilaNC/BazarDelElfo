d0703b978bdeb69a2c48602ae606277d0bd2eda0
PriscilaNC <p.navarrete06@ufromail.cl>
2023-06-09 15:01:23 -0400

    marketplace

--- src/components/card-producto.vue
+++ src/components/card-producto.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <div class="card" style="width: 18rem;" v-if="product">
+  <img v-src="{{ product.images[0]}}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Title</h5>
-    <p class="card-text">$0</p>
+    <h5 class="card-title">{{ product.name }}</h5>
+    <p class="card-text">{{ product.price }}</p>
   </div>
 </div>
 </template>
 
 <script>
+import datos from '../assets/json/hardcode-data.json'
 export default {
 
+    name: 'TableProducts',
+
+    computed: {
+    products() {
+        return datos.map((product) => {
+            return product;
+        })
+    }
 }
+}
+
 </script>
 
 <style>
--- src/pages/MarketPlace.vue
+++ src/pages/MarketPlace.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    v-if
+  <div v-if="product">
+    <Cardproduct/>
   </div>
 </template>
 
