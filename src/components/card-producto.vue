14b940b8fb50d6734dba61bb449767c5b9cdaaa6
PriscilaNC <p.navarrete06@ufromail.cl>
2023-06-08 23:34:26 -0400

    componentes

--- src/components/card-producto.vue
+++ src/components/card-producto.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Title</h5>
+    <p class="card-text">$0</p>
+  </div>
+</div>
 </template>
 
 <script>
