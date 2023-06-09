f6f279329e3562407fbd3c4ca947154c9e1c3884
jabinherrera <j.herrera20@ufromail.cl>
2023-06-09 14:23:24 -0400

    create product

---
+++ src/components/create-product.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="mb-3">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre producto">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text">$</span>
+        <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="mb-3">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
