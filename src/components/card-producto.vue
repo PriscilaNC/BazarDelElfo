518ce7ad324f058ee480a6947e1298b512779f77
PriscilaNC <p.navarrete06@ufromail.cl>
2023-06-09 15:14:52 -0400

    marketplace

--- src/components/card-producto.vue
+++ src/components/card-producto.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card" style="width: 18rem;" v-if="product">
-  <img v-src="{{ product.images[0]}}" class="card-img-top" alt="...">
+  <!-- <img v-src="{{ product.images[0]}}" class="card-img-top" alt="..."> -->
   <div class="card-body">
     <h5 class="card-title">{{ product.name }}</h5>
     <p class="card-text">{{ product.price }}</p>
--- src/pages/MarketPlace.vue
+++ src/pages/MarketPlace.vue
@@ -1,12 +1,16 @@
 <template>
-  <div v-if="product">
-    <Cardproduct/>
+  <div >
+    <Cardproduct v-for="product in products" v-bind:key="product.id"/>
   </div>
 </template>
 
 <script>
+import {products} from '../components/card-producto.vue';
+import CardProduct from '../components/card-producto.vue';
+
 export default {
-  name: 'MarketPlace'
+  name: 'MarketPlace',
+  components: { CardProduct}
 }
 </script>
 
